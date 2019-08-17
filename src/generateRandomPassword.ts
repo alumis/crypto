3c744c7bae4c5dbeddea795d80b3d3c737076fb3
Emil Müller <emil@alumis.no>
2019-08-17 23:45:21 +0200

    bumped

--- src/generateRandomPassword.ts
+++ src/generateRandomPassword.ts
@@ -1,12 +1,12 @@
 import { Cipher } from "./Cipher";
 import { NotSupportedError } from "@alumis/utils/src/NotSupportedError";
-import { uint8ArrayToBase64 } from "@alumis/utils/src/uint8ArrayToBase64";
+import { Uint8ArrayToBase64 } from "@alumis/utils/src/Uint8ArrayToBase64";
 
 /** Generates a random password which strength matches the given cipher */
 export function generateRandomPassword(cipher = Cipher.AES256) {
 
     if (cipher === Cipher.AES256)
-        return uint8ArrayToBase64(crypto.getRandomValues(new Uint8Array(32)));
+        return Uint8ArrayToBase64(crypto.getRandomValues(new Uint8Array(32)));
 
     throw new NotSupportedError();
 }
